1c2c121b4e41d734207aacdb1d057b881aa55b45
CptButtocks <mr.ronanvonk@gmail.com>
2016-09-04 21:29:45 +0200

    Db reference file added

--- src/Controller/DatabaseController.java
+++ src/Controller/DatabaseController.java
@@ -1,8 +1,8 @@
 package Controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import Logic.Product;
+
+import java.sql.*;
 
 /**
  * Created by RonOS on 9/4/2016.
@@ -32,4 +32,35 @@ public class DatabaseController {
             throw new IllegalStateException("Cannot connect the database!", ex);
         }
     }
+
+    public Product getProductByCode(int productCode) {
+        //TODO query to database to retrieve product
+        try {
+            Statement stmt = KassaConnection.createStatement();
+            ResultSet result = stmt.executeQuery("SELECT FROM product WHERE id=\"" + productCode);
+            int price = 0;
+            int id = 0;
+            String name = "";
+            String description = "";
+            String allergy = "";
+            while (result.next())
+            {
+                id = result.getInt("id");
+                name = result.getString("name");
+                price = result.getInt("price");
+                description = result.getString("description");
+                allergy = result.getString("allergy");
+
+
+            }
+
+            return new Product(id, name, price, allergy, description);
+        }
+        catch (SQLException ex) {
+            throw new IllegalStateException("Cannot execute query", ex);
+        }
+
+    }
+
+
 }
