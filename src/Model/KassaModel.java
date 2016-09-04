34459884c10ff6a8c39c23f42ebf38a99f0959ea
Vincent Luder <vincentluder@hotmail.com>
2016-09-04 19:32:26 +0200

    nee

---
+++ src/Model/KassaModel.java
@@ -0,0 +1,13 @@
+package Model;
+
+/**
+ * Created by vincent on 4-9-2016.
+ */
+public class KassaModel {
+    //TODO  sql connectie
+    //TODO Producten toevoegen
+    //TODO Kiloprijs
+    //TODO lijst
+
+    private
+}
