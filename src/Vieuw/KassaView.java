7c4f0ec94f8ed2e423351d17f7b7c9931c2a2209
redvox27 <v.t.luder@st.hanze.nl>
2016-09-05 23:37:08 +0200

    henk

--- src/Vieuw/KassaView.java
+++ src/Vieuw/KassaView.java
@@ -1,14 +1,16 @@
 package Vieuw;
 
 import Controller.KassaController;
+import Logic.Employee;
 import Model.KassaModel;
 
 import javax.swing.*;
+import javax.swing.text.html.ListView;
 
 /**
  * Created by RonOS on 9/4/2016.
  */
-public class KassaView extends JFrame{
+public class KassaView extends JPanel{
     //TODO Create views and add listeners to controller.
     //TODO HET VINCENT, TODO HET.
 
@@ -16,9 +18,17 @@ public class KassaView extends JFrame{
     protected KassaController controller;
 
 
+    private DefaultListModel<Employee> employeeDefaultListModel = new DefaultListModel<Employee>();
+    private JList employeeListView = new JList(employeeDefaultListModel);
+
+
     public KassaView(KassaModel model, KassaController controller){
         this.model = model;
         this.controller = controller;
+        employeeDefaultListModel.addElement(new Employee(1, "vincent"));
+
+        employeeListView.repaint();
+        repaint();
     }
 
     public KassaModel getModel(){
